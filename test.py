c1765711e9d406ffa757fec5deeabf8752637f82
eklavyapopli-ship-it <eklavayapopli@gmail.com>
2025-09-29 08:39:48 +0530

    updated

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import barcode
 from barcode.writer import ImageWriter
 load_dotenv()
-client=MongoClient(os.getenv("DATABASE"))
+client=MongoClient(os.getenv('DATABASE'))
 db=client['employee']
 collection=db['inventory']
 
@@ -17,19 +17,23 @@ for i in range(1,num+1):
 
     print(f"Give details for Product {i}")
     name=input("Enter name of product: ")
-    NamePre=name.removesuffix(name[1:len(name)])
+    if len(name)>=3:
+
+        NamePre=name[0:3]
+    else:
+        NamePre=name
     list1.append(NamePre)
     n=int(input("How many categories?: "))
     for k in range(1,n+1):
         CatType=input(f"enter category {k} type: ")
         category=input(f"Enter Category{k} ")
-        CatPre=category.removesuffix(category[2:len(category)])
-        list1.append(   CatPre)
+        CatPre=category.removesuffix(category[3:len(category)])
+        list1.append(CatPre)
         list2.append(category)
         list3.append(CatType)
         seperator="-"
-        joining=seperator.join(list1)
-        joining1=seperator.join(list3)
+        SKU_id=seperator.join(list1)
+        format=seperator.join(list3)
     db.inventory.insert_many(
         [{
             "name":name,
@@ -39,9 +43,10 @@ for i in range(1,num+1):
     )
     list1.clear()
     list2.clear()
-    print(f"the format of SKU is ProductName-{joining1}")
-    print(f"the sku id is {joining}")
-    data = joining
+    list3.clear()
+    print(f"the format of SKU is ProductName-{format}")
+    print(f"the sku id is {SKU_id}")
+    data = SKU_id
 
     barcode_class = barcode.get_barcode_class('code128')
 
